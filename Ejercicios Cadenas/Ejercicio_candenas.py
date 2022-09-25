a43c7e3fe70e6ba46c0ea2a0039ea6cc77686a55
christianM <cristianmg3099@gmail.com>
2022-09-25 16:54:16 -0600

    Inician los ejercicios de cadenas

---
+++ Ejercicios Cadenas/Ejercicio_candenas.py
@@ -0,0 +1,13 @@
+from typing_extensions import Self
+
+
+class Cadenas:
+
+    def __init__(self,cadena) :
+        self.cadena = cadena
+        
+        
+
+    def Ejercicio_1(self,n) -> str:
+        #Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
+        return self.cadena * n
