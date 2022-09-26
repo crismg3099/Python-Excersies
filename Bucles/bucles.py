5a343865a6dd74412b3d7002366a43f5888263b2
christianM <cristianmg3099@gmail.com>
2022-09-25 18:48:03 -0600

    Ejercicio 2 listo

--- Bucles/bucles.py
+++ Bucles/bucles.py
@@ -14,5 +14,11 @@ class bucles():
             resultado= resultado + palabra
         
         return  resultado
-            
+    
+    def Ejercicio_2(self,edad:int) -> list:
+        #Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+            tiempo = []
+            for i in range(1,edad+1):
+                tiempo.append(i)
+            return tiempo
 
--- Bucles/test.py
+++ Bucles/test.py
@@ -9,6 +9,12 @@ class pruebas(unittest.TestCase):
         prueba1= objeto_1.Ejercicios_1("hola ")
         resultado = "hola hola hola hola hola hola hola hola hola hola "
         self.assertEqual(prueba1,resultado)
+    
+    def test_ejercicio_2(self): 
+        objeto_2 = bucles.bucles()
+        prueba2= objeto_2.Ejercicio_2(23)
+        resultado = [i for i in range(1,23+1)]
+        self.assertEqual(prueba2,resultado)
 
 
 
